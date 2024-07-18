e763543a24064bb29ab58f98ad9dbbdd61daae98
Pedro <pedroaugustogoncalvesribeiro@gmail.com>
2024-07-18 13:57:24 -0300

    Desafio diagrama UML da DIO

---
+++ src/aparelhotelefonico/AparelhoTelefonico.java
@@ -0,0 +1,16 @@
+package aparelhotelefonico;
+
+public class AparelhoTelefonico {
+    
+    public void ligar(){
+        System.out.println("Iniciando ligação!");
+    }
+
+    public void atender(){
+        System.out.println("Atendendo ligação!");
+    }
+
+    public void iniciarCorreioVoz(){
+        System.out.println("Iniciando correio de voz!");
+    }
+}
---
+++ src/navegador/NavegadorNaInternet.java
@@ -0,0 +1,16 @@
+package navegador;
+
+public class NavegadorNaInternet {
+    
+    public void exibirPagina(){
+        System.out.println("Exibindo Página!");
+    }
+
+    public void adicionarNovaAba(){
+        System.out.println("Adicionando nova aba!");
+    }
+
+    public void atualizarPagina(){
+        System.out.println("Atualizando página!");
+    }
+}
